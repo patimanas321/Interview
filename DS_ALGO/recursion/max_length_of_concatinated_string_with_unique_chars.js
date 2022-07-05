3d688acb25f644d067b9612b66fa9e34db006626
Manas Pati <patimanas321@yahoo.com>
2022-07-05 14:56:20 +0530

    Issue fixes

--- DS_ALGO/recursion/max_length_of_concatinated_string_with_unique_chars.js
+++ DS_ALGO/recursion/max_length_of_concatinated_string_with_unique_chars.js
@@ -5,39 +5,29 @@
  var maxLength = function(arr) {
     let maxLength = 0;
     
-    const traverse = (words, prefix = {length: 0, tracker: {}}) => {
+    const traverse = (words, prefix = '') => {
         if (words.length === 0) {
             maxLength = Math.max(maxLength, prefix.length);
             return;
         }
         
         const word = words[0];
-        if (!hasOverlappingChars(word, prefix.tracker)) {
-            const tracker = {...prefix.tracker};
-            for(let i=0; i < word.length; i++) {
-                tracker[word[i]] = true;
-            }
-            traverse(words.slice(1), {
-                length: prefix.length + word.length,
-                tracker
-            });
+        if (!hasOverlappingChars(word, prefix)) {
+            traverse(words.slice(1), prefix + word);
         }
         traverse(words.slice(1), prefix);
     };
     
     const hasOverlappingChars = (word, tracker) => {
-        for(let i=0; i < word.length; i++) {
-            if (tracker[word[i]]) {
-                return true;
-            }
-        }
-        return false;
+        const concatenated = word + tracker;
+        const concatenatedSet = new Set(concatenated.split(''));
+
+        return concatenatedSet.size !== concatenated.length;
     };
     
     traverse(arr);
     return maxLength;
 };
-
 
 console.log(maxLength(["un","iq","ue"])); // 4
 console.log(maxLength(["cha","r","act","ers"])); // 6
